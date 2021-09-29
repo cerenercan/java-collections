6988c560ad8b4ac4823a72ea12016c06c24ced57
cerenercan <36184074+cerenercan@users.noreply.github.com>
2021-09-29 18:23:57 +0300

    ArrayList example

---
+++ java-generics/src/com/company/GenericArrayLists.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GenericArrayLists {
+    public static void mainGenericArrayLists(){
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+        printDoubles(items);
+    }
+
+    private static void printDoubles(ArrayList<Integer> items){
+        for (int i : items){
+            System.out.println(i * 2);
+        }
+    }
+}
